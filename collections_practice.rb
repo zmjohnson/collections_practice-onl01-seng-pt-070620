d66e5dd30d2a28dfc264b98f19887998d8c5c9fc
Zachary Johnson <zacharymjohnson2@gmail.com>
2020-07-11 18:47:32 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,3 +9,14 @@ end
 
 test_array = [1,2,3,4]
 string_test = ["blake", "ashley", "scott"]
+
+def swap_elements_from_to(array, index, new_index)
+  array[index], array[new_index] = array[new_index], array[index]
+  return array
+end
+
+describe 'swap_elements_from_to' do
+  it 'swaps elements and allows you to specify the index of the element you would like to move to a new index' do
+    expect(swap_elements_from_to(["one", "two", "three"], 2, 1)).to eq(["one", "three", "two"])
+  end
+end
